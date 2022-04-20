e9d95bf9f16436405a74ecb944db594bba51ce42
erik_cesar11@hotmail.com <erik_cesar11@hotmail.com>
2022-04-20 11:17:43 -0300

    Fix

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-app.set('view engine', 'html')
-app.get('/', async (req, res) => res.render('mainPage/main'))
+
+app.get('/', async (req, res) => {
+    res.render('mainPage/main.ejs')
+})
 
 app.get('/ativo/:ativo', async (req, res) => {
     const { ativo } = req.params
